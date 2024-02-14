40577f43b59cafa64ad10a9bc6cdb93c6cb46ddf
Ivan Kiiashko <ivan.m.kiiashko@gmail.com>
2024-02-13 19:26:20 -0500

    3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, rejected) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        return resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        return rejected(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
+
+const formRef = document.querySelector('.form');
+
+const onFormSubmit = e => {
+  e.preventDefault();
+
+  let delay = Number(e.currentTarget.elements.delay.value);
+  const step = Number(e.currentTarget.elements.step.value);
+  const amount = Number(e.currentTarget.elements.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(result => {
+        Notify.success(result);
+      })
+      .catch(err => {
+        Notify.failure(err);
+      });
+
+    delay += step;
+  }
+};
+
+formRef.addEventListener('submit', onFormSubmit);
